85a4c9fb7ba0bb49a245da737074fd84106b0612
Paddy Moran <pdmoran@outlook.com>
2017-02-06 17:18:56 +1300

    Add colour difference between headings and paragraphs

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,6 @@ gulp.task('styles', function() {
     return gulp.src(sassSource)
                .pipe(sass({ errLogToConsole: true }))
                .pipe(autoprefixer('last 2 versions', 'ie 9', 'ios 6', 'android 4'))
-               .pipe(gulp.dest(cssDest))
                .pipe(rename({ suffix: '.min' }))
                .pipe(minifycss())
                .pipe(gulp.dest(cssDest));
