527fd97cc532e3a8cbd237d24d86fb407c709b9c
Paddy Moran <pdmoran@outlook.com>
2017-07-28 21:03:12 +1200

    Fixed hero on safari by fixing auto-prefixer

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var sassSource = './sass/**/*.scss';
 gulp.task('styles', function() {
     return gulp.src(sassSource)
                .pipe(sass({ errLogToConsole: true }))
-               .pipe(autoprefixer('last 2 versions', 'ie 9', 'ios 6', 'android 4'))
+               .pipe(autoprefixer())
                .pipe(rename({ suffix: '.min' }))
                .pipe(minifycss())
                .pipe(gulp.dest(cssDest));
